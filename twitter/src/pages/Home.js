df5f6020ab4edb1ead39daf3d9720212116aa68a
Edouard-Germain <edangermain@gmail.com>
2022-01-18 16:39:32 +0100

    début front

--- twitter/src/pages/Home.js
+++ twitter/src/pages/Home.js
@@ -5,5 +5,17 @@ const Home = () =>{
 
     const {userName,setUserName} = useContext(UserContext)
     const [page,setPage] = useState(1)
+    const [feed, setFeed] = useState([])
 
+    useEffect(() => {
+        fetch(`http://localhost:5000/feed/?page=${page}&page_size=10&sort=date:desc`)
+        .then(response => response.json())
+        .then(data => setFeed(m =>[...data, ...feed]))
+    }, [page,id])
+
+    return (
+        <Container>
+            
+        </Container>
+    )
 }
