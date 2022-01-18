9e61a9184f92dacdd1414b6bc8659523d583eba8
Edouard-Germain <edangermain@gmail.com>
2022-01-18 10:26:34 +0100

    set up

--- twitter/src/App.js
+++ twitter/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import {UserContextProvider} from './context/context'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path ='/'></Route>
+          <Route path ='/users/:id'></Route>
+          <Route path ='/login'></Route>
+          <Route path ='/tweet/:id'></Route>
+        </Routes>
+      </BrowserRouter>
+    </UserContextProvider>
   );
 }
 
---
+++ twitter/src/context/context.js
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react"
+
+const UserContext = createContext({})
+const UserContextProvider = props=> {
+const [userName, setUserName] = useState(null)
+
+const value = {
+    userName,
+    setUserName
+      }
+
+      return (
+        <UserContext.Provider value={value}>
+          {props.children}
+        </UserContext.Provider>
+      )   
+}
+
+export  {
+    UserContext,
+    UserContextProvider
+} 
