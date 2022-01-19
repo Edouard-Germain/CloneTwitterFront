3b543c6398df9fba452c2bf672996ba0f01783a2
Edouard-Germain <edangermain@gmail.com>
2022-01-19 16:08:41 +0100

    formulaire tweet

--- twitter/src/components/createTweet.js
+++ twitter/src/components/createTweet.js
@@ -1,6 +1,17 @@
+import { useFormik } from 'formik';
+import * as Yup from 'yup'
 
 const CreateTweet = () => {
-    
+    const formik = useFormik({
+        initialValues: {
+            content: ""
+          },
+          validationSchema: Yup.object().shape({
+			content: Yup.string()
+            .max(280, "trop long")
+          }),
+          validateOnChange: false
+    })
     return( 
         <div>
             <form>
