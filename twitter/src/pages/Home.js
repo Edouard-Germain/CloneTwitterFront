8b8bd7d9e2faa30ff7ad700bf258f52c0c1dd3fd
Edouard-Germain <edangermain@gmail.com>
2022-01-19 10:14:51 +0100

    sideNavSt1

--- twitter/src/pages/Home.js
+++ twitter/src/pages/Home.js
@@ -8,25 +8,28 @@ const Home = () =>{
     const {userName,setUserName} = useContext(UserContext)
     const [page,setPage] = useState(1)
     const [feed, setFeed] = useState([])
-
+    const handleTest =()=>{
+        console.log("test", feed)
+    }
     useEffect(() => {
         fetch(`http://localhost:5000/feed/`)
         .then(response => response.json())
         .then(data => setFeed(m =>[...data, ...feed]))
-        .then (console.log(feed))
+        // .then(data=>setFeed(data))
     })
-
     return (
-        <div class ="ontainer-fluid p-0">
+        <div class ="container-fluid p-0">
             <div class ="row">
-                <div class ="col-3 mt-3 bg-primary">
+                <div class ="col-4 pl-3 mt-3 bg-primary">
                     <BsTwitter 
                         color="rgb(29, 155, 240)" 
                         fontSize="2em"
                     />
                 </div>
-                <div class ="col-6 bg-danger" ><p>twitter is zee best</p></div>
-                <div class="col-3 bg-primary"></div>
+                <div class ="col-8 bg-danger " >
+                    <p>twitter is zee best</p>
+                    <button onClick ={handleTest}></button>
+                </div>
             </div>
         </div>
     )
