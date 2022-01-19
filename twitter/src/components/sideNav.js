40b6063776f85888e1bcb0203b28343091107108
HeleneSoubeyrand <helenesoubi@gmail.com>
2022-01-19 10:48:15 +0100

    merge test

--- twitter/src/components/sideNav.js
+++ twitter/src/components/sideNav.js
@@ -2,6 +2,7 @@ import {BiHomeSmile} from 'react-icons/bi'
 import {FiUser} from 'react-icons/fi'
 import {FiUsers} from 'react-icons/fi'
 import {IoLogOutOutline} from 'react-icons/io'
+
 const sideNav = ()=>{
 
     return (
