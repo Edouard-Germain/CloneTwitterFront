d0f6ea38e1c9e49fc157edfe5aec5cd76affabab
HeleneSoubeyrand <helenesoubi@gmail.com>
2022-01-20 10:49:33 +0100

    tweetInput

---
+++ twitter/src/components/InputTweet.js
@@ -0,0 +1,97 @@
+import { useContext, useState } from "react";
+import { UserContext } from '../context/context';
+
+import styled from "styled-components";
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import FormGroup from "react-bootstrap/FormGroup";
+
+import { 
+    ButtonBlue,
+} from "../styles/Button";
+
+const InputTweet = (props) => {
+    const { user, setUser } = useContext(UserContext)
+    const [ validated, setValidated ] = useState(false)
+ 
+    const formik = useFormik({
+        initialValues: {
+          tweet: "",
+        },
+        onSubmit: values => {
+          postTweet(values)
+        },
+        validateOnChange: false,
+        validationSchema: Yup.object({
+          tweet: Yup.string()
+            // .required("Une adresse email est requise"),
+        })
+    })
+
+    const postTweet = async values => {
+        const response = await fetch('http://localhost:5000/tweets', {
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+          body: JSON.stringify(values)
+        })
+
+        const data = await response.json()
+        console.log(user)
+    
+        if (data) {
+          setUser(data)
+          navigate('/home')
+          setValidated(true) 
+        } else {
+          alert(response.statusText)
+        }
+    
+      }
+    return (
+        <>
+                <Form 
+                  onSubmit={formik.handleSubmit} 
+                  validated={validated}
+                >
+                  <ButtonBlue  
+                      className="my-3" 
+                      type='submit'
+                    >
+                        Tweet
+                  </ButtonBlue>
+                  
+                  <FormGroup
+                    controlId="tweetInput"
+                  >
+                    <FormControl
+                      type="text" 
+                      name='tweet'
+                      className="mt-5 mb-1" 
+                      placeholder="What's happening ?"
+                      value={formik.values.tweet}
+                      onChange={formik.handleChange}
+                      isInvalid={formik.errors.postTweet}
+                      isValid={validated}
+                    />
+                    {/* {user && <p>Email valide</p>}
+                    {!user && <ErrorText>Email invalide</ErrorText>} */}
+                  </FormGroup>
+
+                  <ButtonBlue  
+                    className="my-3" 
+                    type='submit'
+                  >
+                      Tweet
+                  </ButtonBlue>
+                </Form>
+       
+        </>
+    )
+}
+export default ModalLogin
--- twitter/src/components/ModalSignup.js
+++ twitter/src/components/ModalSignup.js
@@ -36,7 +36,13 @@ const ModalSignup = (props) => {
     const formik = useFormik({
         initialValues: {
           email: "",
-          password: ""
+          password: "",
+          passwordConfirmation: "",
+          username:"",
+          name:"",
+          picture:"",
+          bio:"",
+          website:"",
         },
         onSubmit: values => {
           login(values)
@@ -44,14 +50,18 @@ const ModalSignup = (props) => {
         validateOnChange: false,
         validationSchema: Yup.object({
           email: Yup.string()
-            .required("Une adresse email est requise"),
+            .required("Une adresse email est requise."),
           password: Yup.string()
-            .required("Un mot de passe est requis")
+            .required("Un mot de passe est requis."),
+          passwordConfirmation: Yup.string()
+          .required("Veuillez confirmer votre mot de passe."),
+          username: Yup.string()
+          .required("Un nom d'utilisateur est requis."),
         })
     })
 
     const login = async values => {
-        const response = await fetch('http://localhost:5000/auth/login', {
+        const response = await fetch('http://localhost:5000/auth/signup', {
           method: 'post',
           headers: {
             'Content-Type': 'application/json',
@@ -129,9 +139,52 @@ const ModalSignup = (props) => {
                       />
                       {!validated && 
                       <Form.Control.Feedback type="invalid">
-                          Mot de passe invalide
+                          Mot de passe non valide
                       </Form.Control.Feedback>
                       }
+                    </FormGroup>
+                    <FormGroup
+                      controlId="passwordConfirmationInput"
+                    >
+                      <FormControl
+                        type="password" 
+                        name='passwordConfirmation'
+                        className="mt-5 mb-1" 
+                        placeholder="Confirmez votre mot de passe"
+                        value={formik.values.passwordConfirmation}
+                        onChange={formik.handleChange}
+                        isInvalid={formik.errors.passwordConfirmation}
+                        isValid={validated}
+                      />
+                    </FormGroup>
+                    
+                    <FormGroup
+                      controlId="usernameInput"
+                    >
+                      <FormControl
+                        type="text" 
+                        name='username'
+                        className="mt-5 mb-1" 
+                        placeholder="Nom d'utilisateur"
+                        value={formik.values.username}
+                        onChange={formik.handleChange}
+                        isInvalid={formik.errors.username}
+                        isValid={validated}
+                      />
+                    </FormGroup>
+                    <FormGroup
+                      controlId="pictureFileInput"
+                    >
+                      <FormControl
+                        type="file"
+                        name='pictureUrl'
+                        className="mt-5 mb-1" 
+                        placeholder="Photo de profil"
+                        value={formik.values.picture}
+                        onChange={formik.handleChange}
+                        // isInvalid={formik.errors.p}
+                        // isValid={validated}
+                      />
                     </FormGroup>
                   </ModalBody>
                   <ModalFooter className="mt-5">
